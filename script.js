1ab78ce02d93a276ce56a4beaf91566b1cf8e296
Jezman263 <93796144+Jezman263@users.noreply.github.com>
2026-07-27 19:40:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-setInterval(()=>{
+async function getMarketData(){
 
-console.log(
-"CashCollege AI scanning geopolitical events..."
-)
+try{
 
-},60000);
+let response = await fetch(
+"https://api.marketstack.com/v1/eod/latest?symbols=XAUUSD"
+);
+
+let data = await response.json();
+
+
+document.getElementById("goldTicker").innerHTML =
+"$" + data.data[0].close;
+
+
+}
+
+catch(error){
+
+document.getElementById("goldTicker").innerHTML =
+"Offline";
+
+}
+
+
+}
+
+
+getMarketData();
+
+setInterval(getMarketData,60000);
