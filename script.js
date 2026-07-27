c73a9a8988a030d2e8d7b338e8e52f75cf37796d
Jezman263 <93796144+Jezman263@users.noreply.github.com>
2026-07-27 21:03:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,32 @@ catch(error){
 
 document.getElementById("goldTicker").innerHTML =
 "Offline";
+  let price = 3450;
+
+
+function updateGold(){
+
+price += (Math.random()-0.5);
+
+document.getElementById("goldPrice").innerHTML =
+"$" + price.toFixed(2);
+
+
+}
+
+
+setInterval(updateGold,2000);
+
+
+document.getElementById("news").innerHTML = `
+
+<p>🔴 Middle East tensions detected</p>
+
+<p>🟡 Fed policy uncertainty increasing</p>
+
+<p>🔴 Global risk sentiment elevated</p>
+
+`;
 
 }
 
